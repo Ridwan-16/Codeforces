be556eea0bf07fe4dd1ea856bcbcdfaa36607215
Ridwan Ahmed <ridwan.ahmed500991@gmail.com>
2026-06-20 02:27:21 +0600

    20/7/26

---
+++ B_Tatar_TV_Show.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << "\n";
+#else
+#define dbg(x)
+#endif
+
+void solve() {
+
+    long long n,k;
+    cin>> n>>k;
+
+    string s;
+    cin>>s;
+
+    
+}
+
+int main() {
+    FAST_IO;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
