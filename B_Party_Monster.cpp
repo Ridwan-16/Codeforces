6cb4f53247641709d4a8e036fb4094e07de97a0e
Ridwan Ahmed <ridwan.ahmed500991@gmail.com>
2026-06-20 23:37:20 +0600

    20/7/26

---
+++ B_Party_Monster.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (int i = (a); i < (b); i++)
+
+#ifdef LOCAL
+#define dbg(x) cerr << #x << " = " << x << "\n";
+#else
+#define dbg(x)
+#endif
+
+void solve() {
+
+    ll n,open = 0,close = 0;
+    cin>>n;
+
+    for(ll i = 0;i < n;i++){
+        
+        char ch;
+        cin>>ch;
+
+        if(ch == '(') open++;
+        else close++;
+    }
+
+    if(open == close) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+    FAST_IO;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
